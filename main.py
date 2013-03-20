7fa773bc21a6103bc8f04734f1eab87030fd32f9
rhea <rhea@rhea.(none)>
2013-03-20 04:33:37 -0400

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from lib.bot import Client
+
+
+def main():
+    user = raw_input('Username?: ')
+    nick = raw_input('Nickname?: ')
+    host = raw_input('Host?: ')
+    while True:
+        try:
+            port = int(raw_input('Port?: '))
+            break
+        except ValueError:
+            pass
+    channel = raw_input('Channel?: ')
+    Client(user=user, nick=nick, host=host, port=port, channel=channel)
+
+
+if __name__ == '__main__':
+    main()
