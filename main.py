0324593d7ad2573393aa0e3fe83ffba74c1c0fa8
rhea <rhea@rhea.(none)>
2013-03-20 07:44:34 -0400

    edit

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from lib.client import Client
 
 
 def main():
-    user = raw_input('Username?: ')
-    nick = raw_input('Nickname?: ')
-    host = raw_input('Host?: ')
+    user = raw_input('Username: ')
+    nick = raw_input('Nickname: ')
+    host = raw_input('Host: ')
     while True:
         try:
-            port = int(raw_input('Port?: '))
+            port = int(raw_input('Port: '))
             break
         except ValueError:
             pass
-    channel = raw_input('Channel?: ')
+    channel = raw_input('Channel: ')
     Client(user=user, nick=nick, host=host, port=port, channel=channel)
 
 
