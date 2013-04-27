e29789d52262bd74f94a4b38e8f1f90af00c9124
Tim Konick <konick781@gmail.com>
2013-04-27 11:27:04 -0400

    register with password option

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def main():
     nick = raw_input('Nickname: ')
     host = raw_input('Host: ')
     while True:
+        reg_option = raw_input('Do you wish to register? (y or n): ')
+        if reg_option.lower() == 'n':
+            password = None
+            break
+        if reg_option.lower() == 'y':
+            password = raw_input('Enter your password: ')
+            break
+    while True:
         try:
             port = int(raw_input('Port: '))
             break
@@ -16,7 +24,12 @@ def main():
             print 'Port must be an integer!'
             pass
     channel = raw_input('Channel: ')
-    Client(user=user, nick=nick, host=host, port=port, channel=channel)
+    Client(user=user, 
+           nick=nick, 
+           host=host, 
+           port=port, 
+           channel=channel, 
+           password=password)
 
 
 if __name__ == '__main__':
