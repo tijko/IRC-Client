691759b887d3721caa877cefcd7d43da9ed5e844
Timmy Konick <konick781@gmail.com>
2013-06-13 00:19:12 -0400

    server line

--- irc-client_ui/lib/responses.py
+++ irc-client_ui/lib/responses.py
@@ -11,14 +11,24 @@ class Response(object):
         self.nick = nick
         self.server = server
 
+    @property
+    def server_line(self):
+        self.screen.insert(END, "Server          | ")
+        pos = float(self.screen.index(END)) - 1
+        self.screen.tag_add("server", str(pos), str(pos + 0.16))
+        self.screen.tag_config("server", background="green2",
+                                         foreground="black")
+
     def whois_user_repl(self, user_data):
         server_repl = 'User: %s' % (user_data[1] + '@' + user_data[2] + '\n')
+        self.server_line
         self.screen.insert(END, server_repl)
         self.screen.see(END)
 
     def whois_chan_repl(self, chan_data):
         server_repl = ('Real Name: %s \nServer: %s\n' %
                       (chan_data[0], chan_data[2].strip(':')))
+        self.server_line
         self.screen.insert(END, server_repl)
         self.screen.see(END)
 
@@ -29,6 +39,7 @@ class Response(object):
             server_repl = ('Users available on %s:\n' + '=' * 25 + '\n') % userlist[1]
             for usr in userlist[2:]:
                 server_repl += ('  ~' + usr.strip(':@') + '\n')
+            self.server_line
             self.screen.insert(END, server_repl)
             self.screen.see(END)
 
@@ -36,6 +47,7 @@ class Response(object):
         skips = [self.server, self.nick, ':', '371', (':' + self.server)]
         if len(server_data) > 2:
             server_data = ' '.join(i for i in server_data if i not in skips).split(' :')
+            self.server_line
             for i in server_data:
                 if '.freenode.net' in i and i[:3] != 'irc':
                     pass
@@ -50,24 +62,29 @@ class Response(object):
         else:
             link_info = 'HOPS: ' + link_info
         if '365' in server_data:
+            self.server_line
             self.screen.insert(END, link_info + '\n')
             self.screen.see(END)
             return
+        self.server_line
         self.screen.insert(END, link_info + '\n')
         self.screen.see(END)
 
     def perm_denied_repl(self, server_response):
         server_response = ' '.join(server_response).strip(':')
+        self.server_line
         self.screen.insert(END, server_response + '\n')
         self.screen.see(END)
 
     def rate_lim_repl(self, server_response):
         server_response = ' '.join(server_response[1:]).strip(':')
+        self.server_line
         self.screen.insert(END, server_response + '\n')
         self.screen.see(END)
 
     def server_com_repl(self, server_coms):
         skips = [self.nick, ':' + self.server]
+        self.server_line
         for com in [i for i in server_coms if i not in skips]:
             if not com.strip(':').isdigit():
                 self.screen.insert(END, '    > %s\n' % com)
@@ -75,32 +92,39 @@ class Response(object):
 
     def server_con_repl(self, server_connections):
         server_connections = ' '.join(server_connections)
+        self.server_line
         self.screen.insert(END, server_connections + '\n')
         self.screen.see(END)
 
     def server_utme_repl(self, server_data):
         server_data = ' '.join(server_data[:9]).strip(':')
+        self.server_line
         self.screen.insert(END, server_data + '\n')
         self.screen.see(END)
 
     def clnt_auth_repl(self, client_data):
         client_data = ' '.join(client_data)
+        self.server_line
         self.screen.insert(END, client_data + '\n')
         self.screen.see(END)
 
     def server_ver(self, server_data):
+        self.server_line
         self.screen.insert(END, 'Server Version: %s\n' % server_data[0])
         self.screen.see(END)
 
     def server_aux(self, server_data):
+        self.server_line
         self.screen.insert(END, ' '.join(server_data) + '\n')
         self.screen.see(END)
 
     def chan_topic(self, topic):
+        self.server_line
         self.screen.insert(END, 'Topic for %s\n' % (' '.join(topic)))
         self.screen.see(END)
 
     def nick_inuse(self, msg):
+        self.server_line
         self.screen.insert(END, ' '.join(msg[:6]) + '\n')
         self.screen.insert(END, 'Use the /NICK <nick> command to choose a new nick\n')
         self.screen.see(END)
@@ -110,6 +134,7 @@ class Response(object):
         whowas_msg = ' '.join(i for i in server_data[:-3] if i not in skips)
         pos = 0
         msg = whowas_msg.split(' :')
+        self.server_line
         while pos < len(msg):
             out = ('\n' + msg[pos].split()[0] +
                    ' ' + ''.join(msg[pos + 1]) +
@@ -120,6 +145,7 @@ class Response(object):
 
     def list_repl(self, list_data):
         topic = ' '.join(list_data)
+        self.server_line
         self.screen.insert(END, "--" + topic.split(' :')[0] + "--\n")
         self.screen.insert(END, topic.split(' :')[1] + '\n')
         self.screen.see(END)
