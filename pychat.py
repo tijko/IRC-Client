bec629b8981c2f8c6b6c50b284ca7a74a01a1c9d
Tim Konick <konick781@gmail.com>
2015-11-10 22:03:00 -0500

    rename main

---
+++ pychat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lib.login import Login
+
+
+def pychat():
+    login = Login
+    login()
+                    
+
+if __name__ == '__main__':
+    pychat()
