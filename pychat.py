4744f3b3e5193ad66a4bba64d8a8d8c4e328fdcc
Tim Konick <konick781@gmail.com>
2015-11-14 16:28:33 -0500

    Rework to have a central root window controlled from a top class

--- pychat.py
+++ pychat.py
@@ -2,12 +2,34 @@
 # -*- coding: utf-8 -*-
 
 from lib.login import Login
+from lib.client import Client
+from Tkinter import *
 
 
-def pychat():
-    login = Login
-    login()
+class PyChat(object):
+
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("300x275+400+100")
+
+    def login(self):
+        self.login = Login(self.root, self.create_client)
+
+    def run(self):
+        self.root.mainloop()
+
+    def create_client(self):
+        credentials = self.login.login_credentials()
+        credentials['root'] = self.root
+        self.reset()
+        self.client = Client(**credentials)
+
+    def reset(self):
+        for element in self.root.winfo_children():
+            element.destroy()
                     
 
 if __name__ == '__main__':
-    pychat()
+    pychat = PyChat()
+    pychat.login()
+    pychat.run()
