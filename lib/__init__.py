18b6d514bdcd6f6f51f4dd15abc494dce2fbb484
Tim Konick <konick781@gmail.com>
2016-02-20 23:18:05 -0500

    Import tkinter functions

---
+++ lib/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from tkinter import Tk, Entry, Label, Button, Scrollbar, Checkbutton,\
+                        IntVar, Text, WORD, END, N, S, E, W
+except ImportError:
+    from Tkinter import Tk, Entry, Label, Button, Scrollbar, Checkbutton,\
+                        IntVar, Text, WORD, END, N, S, E, W
