388d883742e9e1f69949670d30d72e595f32654b
Tim Konick <konick781@gmail.com>
2016-02-20 23:18:34 -0500

    Import tkinter fucntions from lib

--- lib/login.py
+++ lib/login.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Tkinter import *
 from client import Client
 import ConfigParser
 import os
+
+from lib import Entry, Label, Button, Checkbutton, IntVar, END
 
 
 class Login(object):
