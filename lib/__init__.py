e44ec99805e65272e4cfc2dd0a7140ae9a3ba352
Tim Konick <konick781@gmail.com>
2016-02-25 16:28:18 -0500

    Import NORMAL

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,8 @@
 try:
-    from tkinter import Tk, Entry, Label, Button, Scrollbar, Checkbutton,\
-                        TclError, IntVar, Text, WORD, CURRENT, END, N, S, E, W
+    from tkinter import Tk, Entry, Label, Button, Scrollbar,\
+                        Checkbutton, TclError, IntVar, Text,\
+                        NORMAL, DISABLED, WORD, CURRENT, END, N, S, E, W
 except ImportError:
-    from Tkinter import Tk, Entry, Label, Button, Scrollbar, Checkbutton,\
-                        TclError, IntVar, Text, WORD, CURRENT, END, N, S, E, W
+    from Tkinter import Tk, Entry, Label, Button, Scrollbar,\
+                        Checkbutton, TclError, IntVar, Text,\
+                        NORMAL, DISABLED, WORD, CURRENT, END, N, S, E, W
