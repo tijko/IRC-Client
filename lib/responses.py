b26dd3fe0fa069833f82e6d0ec63be79af826345
Tim Konick <konick781@gmail.com>
2016-02-26 16:54:56 -0500

    Make prefix calls through .insert chat window

--- lib/responses.py
+++ lib/responses.py
@@ -9,10 +9,9 @@ from lib import END
 
 class Response(object):
 
-    def __init__(self, screen, nick, prefix):
-        self.screen = screen
+    def __init__(self, chat_window, nick):
+        self.chat_window = chat_window
         self.nick = nick
-        self.prefix_line = prefix
         self.ln_strip = lambda s: s.strip(':')
         self.chan = None
         self.chan_names = defaultdict(list)
@@ -22,19 +21,14 @@ class Response(object):
 
     def whois_user_repl(self, user_data):
         server_repl = 'User: {}@{}\n'.format(user_data[1], user_data[2])
-        self.prefix_line('Server') 
-        self.screen.insert(END, server_repl)
-        self.screen.see(END)
+        self.chat_window._insert('Server', server_repl)
 
     def whois_chan_repl(self, chan_data):
         if len(chan_data) != 2: return
-        self.prefix_line('Server')
         server_repl = 'Real Name: {}\n'.format(chan_data[0])
-        self.screen.insert(END, server_repl)
-        self.prefix_line('Server')
+        self.chat_window._insert('Server', server_repl)
         self.server_repl = 'Server: {}\n'.format(chan_data[1].strip(':'))
-        self.screen.insert(END, server_repl)
-        self.screen.see(END)
+        self.chat_window._insert('Server', server_repl)
 
     def names_repl(self, userlist):
         self.chan = userlist[1] # XXX python3 index error out of range
@@ -43,107 +37,78 @@ class Response(object):
     def end_names_repl(self, server_end):
         if self.comp_chan_names and len(self.chan_names) < 2: return
         if not self.comp_chan_names:
-            self.prefix_line('Server')
             if not self.chan:
-                self.screen.insert(END, 'Channel does not exist\n')
+                self.chat_window._insert('Server', 'Channel does not exist\n')
             else:
-                self.screen.insert(END, 'Total Users in {}: {}\n'.format( 
+                self.chat_window._insert('Server', 'Total Users in {}: {}\n'.format( 
                                    (self.chan, len(self.chan_names[self.chan]))))
-                self.screen.insert(END, '{}\n'.format(' '.join(self.chan_names[self.chan])))
+                self.chat_window._insert('Server', '{}\n'.format(' '.join(self.chan_names[self.chan])))
         else:
             self.comp_chan_names = False
             users = [j for i in self.chan_names.values() for j in i]
             like_users = {i for i in users if users.count(i) > 1}
-            self.prefix_line('Server')
-            self.screen.insert(END, 'Shared users {}\n'.format(len(like_users)))
-            self.screen.insert(END, '{}\n'.format(' '.join(like_users)))
-        self.screen.see(END)
+            self.chat_window._insert('Server', 'Shared users {}\n'.format(len(like_users)))
+            self.chat_window._insert('Server', '{}\n'.format(' '.join(like_users)))
         self.chan = None
         self.chan_names = defaultdict(list)
 
     def info_repl(self, server_data):
         server_info_repl = ' '.join(map(self.ln_strip, server_data))
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(server_info_repl))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(server_info_repl))
 
     def links_repl(self, server_data):
         link_info = ' '.join(server_data[3:])
-        self.prefix_line('Server')
-        self.screen.insert(END, '{}\n'.format(link_info))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(link_info))
 
     def perm_denied_repl(self, server_response):
         server_response = ' '.join(server_response).strip(':')
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(server_responses))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(server_responses))
 
     def rate_lim_repl(self, server_response):
         server_response = ' '.join(server_response[1:]).strip(':')
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(server_responses))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(server_responses))
 
     def server_com_repl(self, server_coms):
         self.server_cmds.append(server_coms[0])
 
     def server_com_end(self, server_end):
-        self.prefix_line('Server')
-        self.screen.insert(END, 'Total Server Commands: {}\n'.format( 
+        self.chat_window._insert('Server', 'Total Server Commands: {}\n'.format( 
                                         len(self.server_cmds)))
-        self.screen.insert(END, '{}\n'.format(self.server_cmds))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(self.server_cmds))
         self.server_cmds = list()
 
     def server_con_repl(self, server_connections):
         server_connections = ' '.join(server_connections)
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(server_connections))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(server_connections))
 
     def server_utme_repl(self, server_data):
         server_data = ' '.join(server_data[:9]).strip(':')
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(server_data))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(server_data))
 
     def clnt_auth_repl(self, client_data):
         client_data = ' '.join(client_data)
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(client_data))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(client_data))
 
     def server_ver(self, server_data):
-        self.prefix_line('Server') 
-        self.screen.insert(END, 'Server Version: {}\n'.format(server_data[0]))
-        self.screen.see(END)
+        self.chat_window._insert('Server', 'Server Version: {}\n'.format(server_data[0]))
 
     def server_aux(self, server_data):
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(' '.join(server_data)))
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(' '.join(server_data)))
 
     def chan_topic(self, topic):
-        self.prefix_line('Server') 
-        self.screen.insert(END, 'Topic for {}\n'.format(' '.join(topic)))
-        self.screen.see(END)
+        self.chat_window._insert('Server', 'Topic for {}\n'.format(' '.join(topic)))
 
     def nick_inuse(self, msg):
-        self.prefix_line('Server') 
-        self.screen.insert(END, '{}\n'.format(' '.join(msg[:6])))
-        self.screen.insert(END, 'Use the /NICK <nick> command to choose a new nick\n')
-        self.screen.see(END)
+        self.chat_window._insert('Server', '{}\n'.format(' '.join(msg[:6])))
+        self.chat_window._insert('Server', 'Use the /NICK <nick> command to choose a new nick\n')
 
     def list_repl(self, list_data):
         topic = ' '.join(list_data)
-        self.prefix_line('Server') 
-        self.screen.insert(END, '-- {} --\n'.format(topic.split(' :')[0]))
+        self.chat_window._insert('Server', '-- {} --\n'.format(topic.split(' :')[0]))
         try:
-            self.screen.insert(END, '{}\n'.format(topic.split(' :')[1]))
+            self.chat_window._insert('Server', '{}\n'.format(topic.split(' :')[1]))
         except IndexError:
             pass
-        self.screen.see(END)
         if self.log_links:
             with open(os.getcwd() + '/.link.txt', 'a') as f:
                 f.write(('=' * 25) + '\n')
